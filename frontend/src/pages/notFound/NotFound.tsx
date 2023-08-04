cb45c66208702b29f88eb0b4e27ac348cc36bb73
LuidiPires <luidihot@gmail.com>
2023-08-04 17:39:17 -0300

    style: Css para a tela Not Found

--- frontend/src/pages/notFound/NotFound.tsx
+++ frontend/src/pages/notFound/NotFound.tsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom'
+import './NotFound.css'
+
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='not-found-container'>
+      <h1 className='not-found-title'>404</h1>
+      <p className='not-found-message'>Página não encontrada</p>
+      <Link className='not-found-button' to='/login'>Voltar</Link>
+    </div>
   )
 }
