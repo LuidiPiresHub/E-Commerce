f480cee8d9ccea75859c9501538749bcd66e41ae
Laura <ldiasvitaltoledo8@gmail.com>
2023-08-14 23:18:28 -0300

    fix: Correção do next

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -2,12 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import productService from '../services/productService';
 import { mapStatus } from '../utils/mapStatus';
 
-const getProducts = async (_req: Request, res: Response, _next: NextFunction): Promise<Response | void> => {
+const getProducts = async (_req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
   try {
     const { type, message } = await productService.getProducts();
     return res.status(mapStatus(type)).json({ message });
   } catch (error) {
-    return _next(error);
+    return next(error);
   }
 };
 
