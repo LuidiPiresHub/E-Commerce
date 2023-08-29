81da0b981bbf4d631d56483a76cef934a3f57820
Laura <ldiasvitaltoledo8@gmail.com>
2023-08-29 17:59:48 -0300

    fix: Url da imagem

--- backend/src/database/seeders/02-products.ts
+++ backend/src/database/seeders/02-products.ts
@@ -6,7 +6,7 @@ export default {
       {
         product_name: 'NVIDIA',
         product_url: `https://www.nvidia.com/content/dam/en-zz/Solutions/
-        geforce/ada/rtx-4090/geforce-ada-4090-web-og-1200x630.jpg`,
+geforce/ada/rtx-4090/geforce-ada-4090-web-og-1200x630.jpg`,
         old_price: 3000,
         discount: 50,
         price: 1500,
@@ -15,7 +15,7 @@ export default {
       {
         product_name: 'GeForce RTX ® Série 40 - NVIDIA',
         product_url: `https://www.nvidia.com/content/dam/en-zz/Solutions/
-        geforce/ada/rtx-4090/geforce-ada-4090-web-og-1200x630.jpg`,
+geforce/ada/rtx-4090/geforce-ada-4090-web-og-1200x630.jpg`,
         old_price: 3000,
         discount: 50,
         price: 1500,
@@ -24,7 +24,7 @@ export default {
       {
         product_name: 'AMD Radeon™ RX Série 7000',
         product_url: `https://www.nvidia.com/content/dam/en-zz/Solutions/
-        geforce/ada/rtx-4090/geforce-ada-4090-web-og-1200x630.jpg`,
+geforce/ada/rtx-4090/geforce-ada-4090-web-og-1200x630.jpg`,
         old_price: 3000,
         discount: 50,
         price: 1500,
