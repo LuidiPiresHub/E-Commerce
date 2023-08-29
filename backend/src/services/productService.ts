ad42eb8a5ff7ecd8c01910888bf11d6c6088873b
Laura <ldiasvitaltoledo8@gmail.com>
2023-08-29 19:46:00 -0300

    fix: Correção de bug

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -4,7 +4,7 @@ import { IService } from '../interfaces/products.interface';
 const getProducts = async (): Promise<IService> => {
   const data = await Products.findAll();
   const products = data.map(({ dataValues }) => dataValues);
-  if (products) {
+  if (products.length) {
     return { type: 'OK', message: products };
   }
   return { type: 'NOT_FOUND', message: 'Product not found' };
